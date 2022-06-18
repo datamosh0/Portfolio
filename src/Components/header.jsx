5e5acccd7af5c29ec5e533e5b44d427fd17f7f17
datamosh0 <dawsoncontreras@gmail.com>
2022-06-18 04:54:02 -0500

    commit

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
       <div className=" pt-30 " style={{ paddingLeft: "3rem" }}>
         <div className="row justify-content-lg-between ">
           <div className="col-lg-12 px-0 ">
-            <div className="menu-area" style={{ marginRight: "18%" }}>
+            <div className="menu-area" style={{ marginRight: "5%" }}>
               <Navbar collapseOnSelect expand="lg" className="p-0">
                 <Navbar.Toggle aria-controls="responsive-navbar-nav">
                   <i className="text-white">
