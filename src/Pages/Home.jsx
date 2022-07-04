cabc6e42b6fa1d67f5747a6d97af1ad3c51b21e8
datamosh0 <dawsoncontreras@gmail.com>
2022-07-04 00:11:50 -0500

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import Header from "../Components/Header";
 import Slider from "../Components/Slider";
 import Projects from "../Components/Projects";
 import Blogs from "../Components/Blogs";
 import Contact from "../Components/Contact";
-import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import "../index.css";
 
